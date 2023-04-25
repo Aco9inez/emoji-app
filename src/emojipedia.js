6dde66d2ae1703cbbece944feb9b10829ad02220
AyoDeji Olojede <aco_9inez@MacBook-Pro.localdomain>
2023-04-25 13:45:28 +0100

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,25 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪🏾 🏋🏾‍♂️",
+      name: "Lifting Weights at the gym",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports. Love feeling the pump after lifting weights"
+    },
+    {
+      id: 2,
+      emoji: "🏀 ⛹️‍♂️",
+      name: "Basketball",
+      meaning:
+        "Major NBA Fan love plauing basketball scoring three pointers are very satisfying as well as dunking a basketball :)"
+    },
+    {
+      id: 3,
+      emoji: "⚽ 🥅",
+      name: "Football",
+      meaning:
+        "Who doesnt LOVE Football love sprinting and running down the wing! I'm an avid Arsenal Fan“."
+    }
+  ];
+  export default emojipedia;
+  
